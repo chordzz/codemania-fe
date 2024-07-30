ab47384493e8058afe5b5995aaea65d62da60686
Olaife Olawore <olaifeolawore@gmail.com>
2024-07-30 12:01:36 +0100

    added fixtures page

--- src/app/dashboard/tournaments/tournament/fixtures/page.tsx
+++ src/app/dashboard/tournaments/tournament/fixtures/page.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
 
+import Fixtures from '@/../public/fixtures-img.png'
 
 export default function Page() {
     return(
-        <div>Tournament Fixtures</div>
+        <div className="mt-4 max-w-full">
+            <div>
+                <Image src={Fixtures} alt="fixtures" />
+            </div>
+        </div>
     )
 }
