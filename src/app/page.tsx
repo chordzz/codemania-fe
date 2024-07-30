289b065f947b9b476b5e32feafae6e4bc138086c
Olaife Olawore <olaifeolawore@gmail.com>
2024-07-30 11:53:45 +0100

    added main page routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="text-white">
-      Main Page
+      <Link href={'/dashboard/tournaments'}>
+        <button className="px-8 py-4 bg-green-500">Click to go to Dashboard</button>
+      </Link>
     </main>
   );
 }
